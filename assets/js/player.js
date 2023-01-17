13df0f55865955334f33fe967e8f899863646f64
Carlos <87106034+Carlosgt502@users.noreply.github.com>
2023-01-17 15:57:34 -0600

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -8,6 +8,6 @@ const userLang = data.userLang;
 const episodeId = data.episodeId;
 const videoConfig = data.videoConfig;
 
-const stream = videoConfig.streams.find(stream=>stream.hardsub_locale==UserLang);
+const stream = videoConfig.streams.find(stream=>stream.hardsub_locale==userLang);
 console.log(stream)
 });
