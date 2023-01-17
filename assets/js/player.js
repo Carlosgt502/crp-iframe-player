a9b454a355af243cdebb769d117ef832149491f2
Carlos <87106034+Carlosgt502@users.noreply.github.com>
2023-01-17 14:25:43 -0600

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -2,16 +2,4 @@ window.addEventListener('message', e => {
 
 console.log(e.data);
 
-const type = e.data.type;
-
-const events= {
-'next-episode':nextEpisode
-};
-
-if(!events[type]()) return;
-
-function nextEpisode(){
-console.log('Siguiente Episodio')
-}
-
 });
