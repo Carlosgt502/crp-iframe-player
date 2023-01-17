22d4fd4824c00697a0f64a22fd8e2e62325762ec
Carlos <87106034+Carlosgt502@users.noreply.github.com>
2023-01-17 17:18:48 -0600

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -9,5 +9,23 @@ const episodeId = data.episodeId;
 const videoConfig = JSON.parse(data.videoConfig);
 
 const stream = videoConfig.streams.filter(stream=>stream.hardsub_locale==userLang);
-console.log(stream)
+const url = stream[0].url;
+
+const PlayerInstance= jwplayer('player_div');
+PlayerInstance.setup({
+'playlist':[{
+'title':title,
+'image':thumbnail',
+'description':description,
+'file':url
+    }]
+  })
+.on('ready',()=>{
+const container = document.querySelector('.loading_container');
+container.style.display='none';
+})
+.on('time',(e)=>{
+console.log(e)
+const time = e;
+})
 });
