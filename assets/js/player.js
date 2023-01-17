acd3208d7b63d7df8d062959b719d08803875dda
Carlos <87106034+Carlosgt502@users.noreply.github.com>
2023-01-17 15:56:47 -0600

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -7,4 +7,7 @@ const description = data.description;
 const userLang = data.userLang;
 const episodeId = data.episodeId;
 const videoConfig = data.videoConfig;
+
+const stream = videoConfig.streams.find(stream=>stream.hardsub_locale==UserLang);
+console.log(stream)
 });
