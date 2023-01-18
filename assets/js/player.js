bf2410af8186b3150214d1b7a66e4a62b0cde89b
Carlos <87106034+Carlosgt502@users.noreply.github.com>
2023-01-17 22:12:05 -0600

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,47 +1,85 @@
 window.addEventListener('message', e => {
 
-const data = e.data;
-const title = data.title;
-const thumbnail = data.thumbnail;
-const description = data.description;
-const userLang = data.userLang;
-const episodeId = data.episodeId;
+    const data = e.data;
 
-const videoConfig = JSON.parse(data.videoConfig);
-const streamList = videoConfig.streams;
-const searchStreamLang = streamList.find(stream=>stream.hardsub_locale==userLang);
-console.log(searchStreamLang)
-const streamLang = searchStreamLang? userLang: '';
-console.log(streamLang)
-for(const stream of streamList){
-console.log(stream)
-if(stream.hardsub_locale == streamLang){
-const url = stream.url;
-startPlayer(url);
-break;
-}
-}
+    const title = data.title;
 
-function startPlayer(url){
-const playerInstance= jwplayer('player_div');
-playerInstance.setup({
-'playlist':[{
-'title':title,
-'image':thumbnail,
-'description':description,
-'file':url
-    }]
-  })
-.on('ready',()=>{
-const container = document.querySelector('.loading_container');
-container.style.display='none';
+    const thumbnail = data.thumbnail;
 
-const time = localStorage.getItem(videoId);
-if(time)document.getElementsByTagName('video')[0].currentTime=time;
-})
-.on('time',(e)=>{
-const position = e.position;
-localStorage.setItem(episodeId,position)
-})
-}
+    const description = data.description;
+
+    const userLang = data.userLang;
+
+    const episodeId = data.episodeId;
+
+    const videoConfig = JSON.parse(data.videoConfig);
+
+    const streamList = videoConfig.streams;
+
+    const searchStreamLang = streamList.find(stream => stream.hardsub_locale == userLang);
+
+    console.log(searchStreamLang)
+
+    const streamLang = searchStreamLang ? userLang : '';
+
+    console.log(streamLang)
+
+    for (const stream of streamList) {
+
+        console.log(stream)
+
+        if (stream.hardsub_locale == streamLang) {
+
+            const url = stream.url;
+
+            startPlayer(url);
+
+            break;
+
+        }
+
+    }
+
+    function startPlayer(url) {
+
+        const playerInstance = jwplayer('player_div');
+
+        playerInstance.setup({
+
+                'playlist': [{
+
+                    'title': title,
+
+                    'image': thumbnail,
+
+                    'description': description,
+
+                    'file': url
+
+                }]
+
+            })
+
+            .on('ready', () => {
+
+                const container = document.querySelector('.loading_container');
+
+                container.style.display = 'none';
+
+                const time = localStorage.getItem(episodeId);
+
+                if (time) document.getElementsByTagName('video')[0].currentTime = time;
+
+            })
+
+            .on('time', (e) => {
+
+                const position = e.position;
+
+                localStorage.setItem(episodeId, position)
+
+            })
+
+    }
+
 });
