2c9a5fcf1d1dcf0030c49eb42ed013b9e28b9ac5
Carlos <87106034+Carlosgt502@users.noreply.github.com>
2023-01-22 00:10:56 -0600

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,83 +1,83 @@
 window.addEventListener('message', ev => {
-    const loading = document.querySelector('.loading');
+  const loading = document.querySelector('.loading');
 
-    const playerError = document.querySelector('.player-error');
+  const playerError = document.querySelector('.player-error');
 
-    if (loading.style.display == 'none') loading.style.display = 'flex';
+  if (loading.style.display == 'none') loading.style.display = 'flex';
 
-    if(playerError.style.display == 'flex') playerError.style.display = 'none';
+  if (playerError.style.display == 'flex') playerError.style.display = 'none';
 
-    const data = ev.data;
+  const data = ev.data;
 
-    console.log(data);
+  console.log(data);
 
-    const videoConfig = ev.data.videoConfig;
+  const videoConfig = ev.data.videoConfig;
 
-    const streamList = videoConfig.streams;
+  const streamList = videoConfig.streams;
 
-    if (!streamList) return displayError();
+  if (!streamList) return displayError();
 
-    const userLang = data.userLang;
+  const userLang = data.userLang;
 
-    const searchStreamLang = streamList.find(stream => stream.hardsub_locale == userLang);
+  const searchStreamLang = streamList.find(stream => stream.hardsub_locale == userLang);
 
-    const streamLang = searchStreamLang ? userLang : '';
+  const streamLang = searchStreamLang ? userLang : '';
 
-    const title = data.title;
+  const title = data.title;
 
-    const image = data.image
+  const image = data.image
 
-    const description = data.description;
-    
-    const videoId = data.videoId;
+  const description = data.description;
 
-    for (const stream of streamList) {
-        if (stream.hardsub_locale == streamLang) {
-            const url = stream.url;
-            startPlayer(url);
-            break;
-        }
+  const videoId = data.videoId;
+
+  for (const stream of streamList) {
+    if (stream.hardsub_locale == streamLang) {
+      const url = stream.url;
+      startPlayer(url);
+      break;
     }
+  }
 
-    function startPlayer(url) {
-        const playerInstance = jwplayer('player')
-        playerInstance.setup({
-            'playlist': [{
-                'title': title,
-                'image': image,
-                'description': description,
-                'file': url
+  function startPlayer(url) {
+    const playerInstance = jwplayer('player')
+    playerInstance.setup({
+      'playlist': [{
+        'title': title,
+        'image': image,
+        'description': description,
+        'file': url
             }]
-        });
+    });
 
-        playerInstance.on('ready', ev => {
-            console.log(playerInstance)
+    playerInstance.on('ready', ev => {
+      if (localStorage.getItem(videoId)) playerInstance.seek(localStorage.getItem(videoId));
 
-            loading.style.display = 'none';
-        })
+      loading.style.display = 'none';
+    })
 
-        playerInstance.on('time', ev => {
-          const currentTime = ev.currentTime;
-            
-          localStorage.setItem(videoId,currentTime);
-        })
+    playerInstance.on('time', ev => {
+      const currentTime = ev.currentTime;
 
-        playerInstance.on('complete', ev => {
+      localStorage.setItem(videoId, currentTime);
+    })
 
-        })
+    playerInstance.on('complete', ev => {
+      localStorage.removeItem(videoId);
+    })
 
-        playerInstance.on('error', ev => {
-            console.log(ev)
-        })
+    playerInstance.on('error', ev => {
+      console.log(ev)
+    })
 
-        playerInstance.on('setupError', ev => {
-            console.log(ev);
-        })
-    }
+    playerInstance.on('setupError', ev => {
+      console.log(ev);
+    })
+  }
 
-    function displayError() {
-        playerError.style.display = 'flex';
+  function displayError() {
+    playerError.style.display = 'flex';
 
-        loading.style.display = 'none';
-    }
+    loading.style.display = 'none';
+  }
 });
